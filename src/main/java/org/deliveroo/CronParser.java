7918f4bec859af9efcadaa17485d80e5aa9ff85f
black_j4ck <sujeetkumariet1@gmail.com>
2024-06-19 01:10:21 +0530

    Refactoring to make more modular

--- src/main/java/org/deliveroo/CronParser.java
+++ src/main/java/org/deliveroo/CronParser.java
@@ -1,26 +1,24 @@
 package org.deliveroo;
 
-import org.deliveroo.parser.*;
 import org.deliveroo.segments.*;
-
-import java.util.List;
-
-import static org.deliveroo.constants.Separator.*;
 import static org.deliveroo.constants.Columns.*;
 import static org.deliveroo.printer.CronFieldPrinter.printTable;
 
 public class CronParser {
-    private static final CronFieldExpander WILDCARD_EXPANDER = new CronFieldWildcardExpander();
-    private static final CronFieldExpander RANGE_EXPANDER = new CronFieldRangeExpander();
-    private static final CronFieldExpander STEP_EXPANDER = new CronFieldStepExpander();
-    private static final CronFieldExpander INDIVIDUAL_EXPANDER = new CronFieldIndividualExpander();
 
     public void parseAndPrintCronString(String cronString) {
         String[] parts = cronString.split(" ", 6);
         if (parts.length != 6) {
             throw new IllegalArgumentException("Invalid cron string format.");
         }
+        try {
+            parseString(parts);
+        } catch (Exception ex) {
+            System.out.println("Error in parsing the given strings");
+        }
+    }
 
+    private static void parseString(String[] parts) {
         String minutePart = parts[0];
         String hourPart = parts[1];
         String dayOfMonthPart = parts[2];
@@ -28,29 +26,11 @@ public class CronParser {
         String dayOfWeekPart = parts[4];
         String command = parts[5];
 
-        printTable(MINUTE, expandField(new Minute(minutePart)));
-        printTable(HOUR, expandField(new Hour(hourPart)));
-        printTable(DAY_OF_MONTH, expandField(new DaysOfMonth(dayOfMonthPart)));
-        printTable(MONTH, expandField(new Month(monthPart)));
-        printTable(DAY_OF_WEEK, expandField(new DaysOfWeek(dayOfWeekPart)));
+        printTable(MINUTE, new Minute(minutePart).expandField());
+        printTable(HOUR, new Hour(hourPart).expandField());
+        printTable(DAY_OF_MONTH, new DaysOfMonth(dayOfMonthPart).expandField());
+        printTable(MONTH, new Month(monthPart).expandField());
+        printTable(DAY_OF_WEEK, new DaysOfWeek(dayOfWeekPart).expandField());
         printTable(COMMAND, command);
-    }
-
-    private List<String> expandField(Base segment) {
-        CronFieldExpander expander;
-
-        if (segment.getSegment().equals(WILDCARD)) {
-            expander = WILDCARD_EXPANDER;
-        } else if (segment.getSegment().contains(STEP)) {
-            expander = STEP_EXPANDER;
-        } else if (segment.getSegment().contains(COMMA)) {
-            expander = INDIVIDUAL_EXPANDER;
-        } else if (segment.getSegment().contains(RANGE)) {
-            expander = RANGE_EXPANDER;
-        } else {
-            expander = INDIVIDUAL_EXPANDER;
-        }
-
-        return expander.expandField(segment.getSegment(), segment.getMinimumValue(), segment.getMaximumValue());
     }
 }
--- src/main/java/org/deliveroo/segments/Base.java
+++ src/main/java/org/deliveroo/segments/Base.java
@@ -1,10 +1,38 @@
 package org.deliveroo.segments;
 
 import lombok.Getter;
+import org.deliveroo.parser.*;
+
+import java.util.List;
+
+import static org.deliveroo.constants.Separator.*;
 
 @Getter
 public abstract class Base {
+    private static final CronFieldExpander WILDCARD_EXPANDER = new CronFieldWildcardExpander();
+    private static final CronFieldExpander RANGE_EXPANDER = new CronFieldRangeExpander();
+    private static final CronFieldExpander STEP_EXPANDER = new CronFieldStepExpander();
+    private static final CronFieldExpander INDIVIDUAL_EXPANDER = new CronFieldIndividualExpander();
+
     protected Integer minimumValue;
     protected Integer maximumValue;
     protected String segment;
+
+    public List<String> expandField() {
+        CronFieldExpander expander;
+
+        if (this.getSegment().equals(WILDCARD)) {
+            expander = WILDCARD_EXPANDER;
+        } else if (this.getSegment().contains(STEP)) {
+            expander = STEP_EXPANDER;
+        } else if (this.getSegment().contains(COMMA)) {
+            expander = INDIVIDUAL_EXPANDER;
+        } else if (this.getSegment().contains(RANGE)) {
+            expander = RANGE_EXPANDER;
+        } else {
+            expander = INDIVIDUAL_EXPANDER;
+        }
+
+        return expander.expandField(this.getSegment(), this.getMinimumValue(), this.getMaximumValue());
+    }
 }
