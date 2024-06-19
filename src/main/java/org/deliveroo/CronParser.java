cb87c8a4568d2a4171b45e58e5a63ad2c5d8db68
black_j4ck <sujeetkumariet1@gmail.com>
2024-06-19 10:07:37 +0530

    Extracting index value to constants

--- src/main/java/org/deliveroo/CronParser.java
+++ src/main/java/org/deliveroo/CronParser.java
@@ -2,12 +2,19 @@ package org.deliveroo;
 
 import org.deliveroo.segments.*;
 import static org.deliveroo.constants.Columns.*;
+import static org.deliveroo.constants.Separator.SPACE;
 import static org.deliveroo.printer.CronFieldPrinter.printTable;
 
 public class CronParser {
+    private static final Integer MINUTE_PART_INDEX = 0;
+    private static final Integer HOUR_PART_INDEX = 1;
+    private static final Integer DAY_OF_MONTH_PART_INDEX = 2;
+    private static final Integer MONTH_PART_INDEX = 3;
+    private static final Integer DAY_OF_WEEK_PART_INDEX = 4;
+    private static final Integer COMMAND_PART_INDEX = 5;
 
     public void parseAndPrintCronString(String cronString) {
-        String[] parts = cronString.split(" ", 6);
+        String[] parts = cronString.split(SPACE, 6);
         if (parts.length != 6) {
             throw new IllegalArgumentException("Invalid cron string format.");
         }
@@ -19,12 +26,12 @@ public class CronParser {
     }
 
     private static void parseString(String[] parts) {
-        String minutePart = parts[0];
-        String hourPart = parts[1];
-        String dayOfMonthPart = parts[2];
-        String monthPart = parts[3];
-        String dayOfWeekPart = parts[4];
-        String command = parts[5];
+        String minutePart = parts[MINUTE_PART_INDEX];
+        String hourPart = parts[HOUR_PART_INDEX];
+        String dayOfMonthPart = parts[DAY_OF_MONTH_PART_INDEX];
+        String monthPart = parts[MONTH_PART_INDEX];
+        String dayOfWeekPart = parts[DAY_OF_WEEK_PART_INDEX];
+        String command = parts[COMMAND_PART_INDEX];
 
         printTable(MINUTE, new Minute(minutePart).expandField());
         printTable(HOUR, new Hour(hourPart).expandField());
